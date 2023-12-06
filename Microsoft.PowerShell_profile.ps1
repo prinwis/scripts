4fb168e8de7bab6d8bd6ff6d1ef9f6de86426502
prinwis <dimenun@gmail.com>
2023-12-06 19:39:26 +0800

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,13 +4,20 @@ $env:Path += ";D:\Programs\platform-tools\;"
 Set-Alias subl "D:\Programs\sublime_text\subl.exe"
 
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadLineOption -BellStyle None
+# Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
 $PSOption =  @{
     PredictionSource = 'History'
     PredictionViewStyle = 'ListView'
 }
+
 Set-PSReadLineOption @PSOption
 Set-PSReadLineOption -Colors @{
     "Error" = "#B00020"
     "String" = "#a6e22e"
+}
 
+function prompt {
+    "$([char]0x1b)[38;2;0;150;255m$(Get-Location)> $([char]0x1b)[0m"
 }
